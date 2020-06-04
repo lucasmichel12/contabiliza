d593ad68dbbb55c057d26212c1505140b3fc8c74
Lucas Michel De Souza <lucas.michel@sicoob.com.br>
2020-06-04 17:07:15 -0300

    Att

--- App/View/_template/footer.php
+++ App/View/_template/footer.php
@@ -13,10 +13,12 @@
         </div>
 </footer>
 <!-- JS -->
-<script src="<?= URL;?>public/jquery/jquery.min.js"></script>
-<script src="<?= URL;?>public/jquery/jqueryMaskMoney.min.js"></script>
-<script src="<?= URL;?>public/popper/popper.min.js"></script>
-<script src="<?= URL;?>public/bootstrap/js/bootstrap.min.js"></script>
-<script src="<?= URL;?>public/jquery/jqueryMatchHeight.min.js"></script>
-<script src="<?= URL;?>public/js/main.js"></script>
+<script src="<?=URL;?>public/jquery/jquery.min.js"></script>
+<script src="<?=URL;?>public/jquery/jquery.flot.js"></script>
+<script src="<?=URL;?>public/jquery/jquery.flot.pie.js"></script>
+<script src="<?=URL;?>public/jquery/jqueryMaskMoney.min.js"></script>
+<script src="<?=URL;?>public/popper/popper.min.js"></script>
+<script src="<?=URL;?>public/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?=URL;?>public/jquery/jqueryMatchHeight.min.js"></script>
+<script src="<?=URL;?>public/js/main.js"></script>
 </body>
