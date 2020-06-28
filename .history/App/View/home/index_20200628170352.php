d6064dc5cc3c61df53a0242d775481e0a7132c45
Larissa Carvalho <larissa.carvalhomorais@gmail.com>
2020-06-28 17:07:48 -0300

    correção de erros frontend

--- .history/App/View/home/index_20200628170352.php
+++ .history/App/View/home/index_20200628170352.php
@@ -6,6 +6,11 @@
             <div class="col-lg-3 col-md-6">
                 <div class="card">
                     <div class="card-body">
+                        <div class="card-left pt-1 float-right">
+                            <h3 class="mb-0 fw-r pad-left-25">
+                                <span class="count">10</span>
+                            </h3>
+                            <p class="text-light mt-1 m-0">Concluidas</p>
                         <div class="stat-widget-five">
                             <div class="stat-icon dib flat-color-1">
                                 <i class="pe-7s-check"></i>
@@ -24,6 +29,13 @@
             <div class="col-lg-3 col-md-6">
                 <div class="card">
                     <div class="card-body">
+<<<<<<< HEAD
+                        <div class="card-left pt-1 float-right">
+                            <h3 class="mb-0 fw-r pad-left-25">
+                                <span class="count">2</span>
+                            </h3>
+                            <p class="text-light mt-1 m-0">Abertas</p>
+=======
                         <div class="stat-widget-five">
                             <div class="stat-icon dib flat-color-2">
                                 <i class="pe-7s-note2"></i>
@@ -34,6 +46,7 @@
                                     <div class="stat-heading">Solicitações em Aberto</div>
                                 </div>
                             </div>
+>>>>>>> frontend
                         </div>
                     </div>
                 </div>
@@ -42,6 +55,13 @@
             <div class="col-lg-3 col-md-6">
                 <div class="card">
                     <div class="card-body">
+<<<<<<< HEAD
+                        <div class="card-left pt-1 float-right">
+                            <h3 class="mb-0 fw-r pad-left-25">
+                                <span class="count">3</span>
+                            </h3>
+                            <p class="text-light mt-1 m-0">Pendentes</p>
+=======
                         <div class="stat-widget-five">
                             <div class="stat-icon dib flat-color-3">
                                 <i class="pe-7s-clock"></i>
@@ -70,6 +90,7 @@
                                     <div class="stat-heading">Funcionários Ativos</div>
                                 </div>
                             </div>
+>>>>>>> frontend
                         </div>
                     </div>
                 </div>
